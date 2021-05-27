54db5e8f7c6d2b38c91fb1fae8baab0d68d2c076
Corrie <corrierthomas@gmail.com>
2021-05-27 01:01:00 -0400

    progress

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,27 +1,63 @@
 // updatePlots = (data) => {
 //     let id = d3.event.target.value;
-//     let sample = data.samples.filter(sample => sample.id === id);
+
 //     let xBubble = sample.otu_ids;
 //     let yBubble = sample.sample_values;
 // }
 
-function metadata() {
-    d3.json("samples.json"), function (data) {
-        let metadata = data.metadata;
-        let sample = metadata.filter(sample => sample.id === id);
-        let demographics = metadata.filter(demographics => demographics.id === sample);
+
+// for demo info: filter metadata for id
+function metadata(sample) {
+    d3.json("../data/samples.json").then(data => {
+        
+        // let demographics = metadata.filter(demographics => demographics.id === mdata_sample);
         let header = d3.select("#sample-metadata");
         header.html("");
-        Object.entries(demographics).forEach(([k, v]) => {
+        Object.entries(demo).forEach(([k, v]) => {
             header.append("option").text(`${k}: ${v}`); //.attr("value", [k, v])
         });
-    }
+    });
 };
 
-function init() {
+function charts() {
+    d3.json("../data/samples.json").then(data => {
+        let samples = data.samples;
+        let csample_results = samples.filter(sample => sample.id === id);
+        let otu = csample_results.map((otus) => otus.otu_ids);
+        let svalue = csample_results.map((svalues) => svalues.sample_values);
+        let olabel = csample_results.map((olabels) => olabels.otu_labels);
+        console.log(otu);
+        console.log(svalue);
+        console.log(olabel);
+    });
+};
 
-d3.json("../data/samples.json").then(data => {
-    console.log(data);
+
+function init() {
+    d3.json("../data/samples.json").then(data => {
+        console.log(data);
+        //for dropdown list: create an option element for all names
+        let mySelect = d3.select("#selDataset");
+        data.names.forEach(element => {
+            mySelect.append("option").attr("value", element).text(element);
+    });
+        //be sure to set the value of the option to your filter value
+        let id = mySelect.property("value");
+        let parsed_id = parseInt(id);
+
+        let sample = data.samples.filter(sample => sample.id === id);
+        console.log(sample);
+        let demo = data.metadata.filter(demographics => demographics.id === parsed_id);
+        console.log(demo);
+
+    
+        mySelect.on("change", () =>
+        updatePlots(data));
+});
+
+
+
+
 
     // filter samples to 940
     // for bubble: 
@@ -33,26 +69,6 @@ d3.json("../data/samples.json").then(data => {
     // xvals are sample_values
     //y vals are otu_ids
 
-    // for demo info: 
-    // filter metadata for id
-
-    //for ddl
-    //create an option element for all names
-    //be sure to set the value of the option to your filter value
-
-    let mySelect = d3.select("#selDataset");
-    // mySelect.append("option").attr("value", "set your value").text("set our text");
-
-    data.names.forEach(element => {
-        mySelect.append("option").attr("value", element).text(element);
-    });
-
-    mySelect.on("change", () =>
-    updatePlots(data));
-});
-
-
-
-};
+  };
 
 init ();
